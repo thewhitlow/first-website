050deb565178ebf32f3cf9df7ab7b883459280dd
Noah <Noahwhitlow312@gmail.com>
2023-11-13 17:24:40 -0500

    linked javascript file

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function changeText(inputName) {
+    document.getElementById("displayedText). innerHTML = 
+
+
+    const myVariableName = (name: "Adam");
+    console.log(myVariableName.name);
+
+    const myList = [];
+    console.log(myList[0)])
+}
