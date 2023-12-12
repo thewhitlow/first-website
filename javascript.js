e7fe3a336e61e063392b0b3646b3b79c84ce2e02
Noah <Noahwhitlow312@gmail.com>
2023-12-12 13:35:47 -0500

    fixed it

--- javascript.js
+++ javascript.js
@@ -1,22 +1,20 @@
 function onPress(inputValue) {
     
-    const url =  https//pokeapi.co/api/v2/pokemon
+    const url =  'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
        if (status === 'success' ) {
         const pokemonNames = data.results.map(pokemon => {
             
         return '<h2>' + pokemon.names + '</h2>' ;
-    });
+        });
         console.log(pokemonNames);
         document.getElementById("output").innerHTML = pokemonNames;
+         } else {
+            console.log("hi")
          }
     });
-
-        
-
-
 }
-
+ 
 function showNames(pokemon) {
     return pokemon;
 }
